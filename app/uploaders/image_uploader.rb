b6e1ca72b8eaf73a9a5cefbd09ef6c80ef00fd47
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-21 06:15:06 +0800

    补充图片上传功能

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
