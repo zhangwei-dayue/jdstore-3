0361427fe8bc3893c3a62e80bf8b9f73a2a55db3
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-21 05:53:48 +0800

    商品可以上传图片

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620213651) do
+ActiveRecord::Schema.define(version: 20170620214412) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170620213651) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
