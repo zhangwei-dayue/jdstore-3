69b4701b0e0dfa9bf1e67b501bb8045615ce70b9
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-21 05:42:58 +0800

    增加商家admin权限

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :edit, :create, :update, :destory]
+  before_action :admin_required
 
   def index
     @products = Product.all
