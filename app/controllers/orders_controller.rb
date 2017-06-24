7e78126b4985add9bbb655a912db4ffee31e0dfd
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-24 16:04:21 +0800

    生成订单后购物车商品清空

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,7 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+    current_cart.clean!
     redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
