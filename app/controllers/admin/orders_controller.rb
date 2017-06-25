d6e8e2d80ae0e7dbbec7aaa82ba47071b2de014f
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-26 05:50:09 +0800

    增加后台订单展示页面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+  layout 'admin'
   def index
     @orders = Order.all.order("id DESC")
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
   end
 end
